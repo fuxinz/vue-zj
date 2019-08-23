e02c8ba8e7290026e943e5ea0b2ad89b73d436f5
zhufuxin <981133064@qq.com>
2019-08-23 11:03:21 +0800

    init

---
+++ examples/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <colorPicker v-model="color" v-on:change="headleChangeColor"></colorPicker>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      color: '#ff0000'
+    }
+  },
+  methods: {
+    headleChangeColor() {
+      console.log('颜色改变')
+    }
+  }
+}
+</script>
